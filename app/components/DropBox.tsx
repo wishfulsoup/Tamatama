af8da54319a86ce14dfa53874989edda62cd67c0
KimKyeongSeog <kimks9241@naver.com>
2024-03-25 13:30:02 +0900

    DropBox layout

--- app/components/DropBox.tsx
+++ app/components/DropBox.tsx
@@ -1,29 +1,74 @@
 "use client";
 
-import { FC, useState } from "react";
+import React, { FC, useState } from "react";
+import items from "./db/items.json";
+import { FaSearch } from "react-icons/fa";
+
+interface Item {
+  name: string;
+  value: string;
+}
 
 const DropBox: FC = () => {
-  const [view, setView] = useState(false);
+  const [view, setView] = useState<boolean>(false);
+  const [selectedValue, setSelectValue] = useState<string>("");
+  const [result, setResult] = useState<Item[]>([]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const resultItems = items[selectedValue] || []; // Check if result exists
+    setResult(resultItems);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectValue(event.target.value);
+  };
 
   return (
-    <div className="bg-gray-500 rounded-2xl h-[5rem] w-[24rem] flex justify-between items-center px-[2rem]">
-      <div>
-        <select className="text-[1.4rem] appearance-none bg-gray-500">
-          <option value="모자">원하는 부위를 선택하세요</option>
-          <option value="상의">모자</option>
-          <option value="상의">상의</option>
-          <option value="하의">하의</option>
-          <option value="신발">신발</option>
-          <option value="장신구">장신구</option>
-        </select>
+    <div className=" h-[64vh]">
+      <div className="bg-gray-600 rounded-2xl h-[6rem] w-[26rem] pl-[1rem] flex items-center">
+        <form className="" onSubmit={handleSubmit}>
+          <select
+            className="text-[1.6rem] appearance-none w-[18rem] mr-[1.0rem] text-center font-bold bg-gray-600 mb-[2rem] mt-[2rem]"
+            onChange={handleChange}
+          >
+            <option value="">Product types</option>
+            <option value="head">Head</option>
+            <option value="top">Top</option>
+            <option value="pants">Pants</option>
+            <option value="shoes">Shoes</option>
+            <option value="accessories">Accessories</option>
+          </select>
+          <button
+            type="submit"
+            onClick={() => {
+              setView(!view);
+            }}
+            className="w-[5rem] h-[3rem] bg-gray-600 relative"
+          >
+            <FaSearch className="absolute w-[3rem] h-[3rem] top-[-0.5rem] ml-[1rem]" />
+          </button>
+        </form>
       </div>
-      <button
-        onClick={() => {
-          setView(!view);
-        }}
-        className="border-l-2 border-gray-300 ml-[0.5] w-[15%] text-2xl"
-      >
-        {view ? "⌃" : "⌄"}
+      <div>
+        {result.length > 0 && (
+          <div>
+            <ul className="flex flex-col items-center overflow-scroll h-[48vh] shop-items-scroll">
+              {result.map((item, index) => (
+                <li
+                  key={index}
+                  className="bg-gray-500 rounded-2xl min-h-[5rem] min-w-[20rem] flex items-center justify-center mt-[5vh]"
+                >
+                  <div>{item.item}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+      <button className="bg-gray-600 rounded-2xl h-[6rem] w-[26rem] flex items-center justify-center mt-[1rem] hover:text-white active:text-white ">
+        Resets
       </button>
     </div>
   );
--- app/marketplace/gacha/page.tsx
+++ app/marketplace/gacha/page.tsx
@@ -6,38 +6,38 @@ const Gacha: FC = () => {
   return (
     <div className="bg-black root-fontstyle">
       <Header />
-      <div className="h-[73vh] text-[1.6rem] text-white bg-header w-[100%] flex items-center justify-center">
-        <div className="overflow-x-auto w-[60%]">
+      <div className="h-[73vh] text-[1.6rem] bg-black w-[100%] flex items-center justify-center">
+        <div className=" w-[60%]">
           <div
-            className="p-[4rem] h-[50vh] w-[100%] bg-white text-black rounded-2xl grid grid-cols-3 gap-[2rem] "
+            className="p-[4rem] h-[50vh] w-[50vw] bg-white text-black rounded-2xl grid grid-cols-3 gap-[2rem]"
             id="gachas"
           >
-            <div className="rounded-2xl flex items-center justify-center col-1-of-3 h-[20vh] bg-gray-400">
+            <div className="rounded-2xl flex items-center justify-center h-[20vh] bg-gray-400">
               Tama
             </div>
 
-            <div className=" rounded-2xl flex items-center justify-center col-1-of-3 h-[20vh] bg-gray-400">
+            <div className="rounded-2xl flex items-center justify-center h-[20vh] bg-gray-400">
               Tama
             </div>
 
-            <div className=" rounded-2xl flex items-center justify-center col-1-of-3 h-[20vh] bg-gray-400">
+            <div className="rounded-2xl flex items-center justify-center h-[20vh] bg-gray-400">
               Tama
             </div>
 
-            <div className="rounded-2xl flex items-center justify-center col-1-of-3 h-[20vh] bg-gray-400">
+            <div className="rounded-2xl flex items-center justify-center h-[20vh] bg-gray-400">
               Tama
             </div>
 
-            <div className=" rounded-2xl flex items-center justify-center col-1-of-3 h-[20vh] bg-gray-400">
+            <div className="rounded-2xl flex items-center justify-center h-[20vh] bg-gray-400">
               Tama
             </div>
 
-            <div className=" rounded-2xl flex items-center justify-center col-1-of-3 h-[20vh] bg-gray-400">
+            <div className="rounded-2xl flex items-center justify-center h-[20vh] bg-gray-400">
               Tama
             </div>
           </div>
         </div>
-        <div className="h-[64vh] ml-[5rem]">
+        <div className="w-[20rem] h-[5rem] ml-[5rem] absolute top-[16vh] right-[7vw] text-white">
           <div>
             재화 1 : 12345 <button>+</button>
           </div>
@@ -45,10 +45,8 @@ const Gacha: FC = () => {
             재화 2 : 12345 <button>+</button>
           </div>
         </div>
-        <button className="mt-[50vh] h-[8rem] w-[12rem] bg-gray-200 text-black rounded-2xl text-right">
-          가챠버튼
-          <br />
-          (1, 10회 뽑기)
+        <button className="w-[13vw] h-[13vh] ml-[5rem] rounded-2xl absolute bottom-[23.5vh] right-[7vw] bg-gray-500 text-[3.0rem]">
+          GACHA!
         </button>
       </div>
       <GachaFooter />
