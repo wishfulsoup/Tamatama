80605946d29fc000cbd4344fe9cce9c2490f8522
HOONKH <boospa0305@gmail.com>
2024-04-02 13:00:26 +0900

    STEAK FIX & DROPMENU ADD & LANDINGPAGE

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ import CashBox from "./CashBox";
 const Header: FC = () => {
   return (
     <header className="bg-transparent text-theme h-[15vh] flex root-font width upper text-theme-primary width">
-      <div className="w-[20%] inline-block">
+      <div className="w-[33.333333%] inline-block">
         <div className="text-[3rem] w-[100%]relative ">
           <div className="flex flex-col items-center top-[3rem] left-[3rem] absolute ">
             <a className="flex flex-col items-center" href="/">
@@ -14,8 +14,8 @@ const Header: FC = () => {
           </div>
         </div>
       </div>
-      <div className="w-[80%] relative inline-block">
-        <div className="flex items-center text-[1.6rem] gap-[13rem] right-[3rem] absolute top-[3.6rem] w-[68%] ">
+      <div className="w-[66.6666667%] relative inline-block">
+        <div className="flex items-center justify-end text-[1.6rem] gap-[13rem] right-[1rem] absolute top-[3.6rem] w-[68%] ">
           <a href="/pets" className="btn btn-line">
             PETS
           </a>
@@ -34,12 +34,19 @@ const Header: FC = () => {
             <label htmlFor="modal-toggle" className="button__modal">
               <span className="button__icon">&nbsp;</span>
             </label>
+            <nav className="modal__nav">
+              <ul className="flex flex-col">
+                <li className="modal__item">HI</li>
+                <li className="modal__item">THERE</li>
+                <li className="modal__item">WASSUP</li>
+              </ul>
+            </nav>
           </div>
         </div>
-        <div className="absolute right-[11rem] top-[12rem]">
-          <CashBox />
-        </div>
       </div>
+      {/* <div className="absolute right-[11rem] top-[12rem]">
+        <CashBox />
+      </div> */}
     </header>
   );
 };
--- app/dressroom/page.tsx
+++ app/dressroom/page.tsx
@@ -6,16 +6,7 @@ const Dressroom: FC = () => {
     <section className="bg-theme h-[100vh] width m-0 p-0">
       <Header />
       <div className="w-[100%] h-[85vh] relative">
-        <div className="absolute right-[2rem] top-0 bg-transparent h-[10vh] w-[10%] text-[1.2rem] text-white text-theme-secondary">
-          <div>
-            <div>
-              재화 1 : 12345 <button>+</button>
-            </div>
-            <div>
-              재화 2 : 12345 <button>+</button>
-            </div>
-          </div>
-        </div>
+        <div className="absolute right-[2rem] top-0 bg-transparent h-[10vh] w-[10%] text-[1.2rem] text-white text-theme-secondary"></div>
       </div>
     </section>
   );
